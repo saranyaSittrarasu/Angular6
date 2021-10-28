57325eba37035d73739cc626aab088c184ec96a1
saranyaSittrarasu <33187397+saranyaSittrarasu@users.noreply.github.com>
2021-10-28 22:31:32 +0200

    First commit

---
+++ GruntFile.js
@@ -0,0 +1,15 @@
+﻿module.exports = function (grunt) {
+    //load task
+    grunt.loadNpmTasks('grunt-shell');
+    //initialise the configuration of grunt
+    grunt.initConfig({
+        config:grunt.file.readJSON('appsettings.json'),
+        shell: {
+            command: ["cd <%= config.angulardir %>",
+                "ng build --deploy-url=/CustomerApp/dist/CustomerApp/ --watch"].join('&&')
+        }
+    })
+    
+    //default task to execute
+    grunt.registerTask('ngbuild', ['shell']);
+}
